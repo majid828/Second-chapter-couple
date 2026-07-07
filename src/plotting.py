0e8826d1d73208d975339febf39ce7aefb1c8073
majid828 <mhshah1@crimson.ua.edu>
2026-07-07 04:43:11 -0500

    Delete src directory

--- src/plotting.py
+++
@@ -1,3 +0,0 @@
-import matplotlib.pyplot as plt
-def make_plots(b,s,t,r):
- plt.plot(t['h'],label='true'); plt.plot(r['h'],label='recovered'); plt.legend(); plt.savefig('results/figures/memory_kernel.png'); plt.close()
