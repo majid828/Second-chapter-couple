271efbc3aabf496a980c0c8c348c808dd3e8850c
majid828 <mhshah1@crimson.ua.edu>
2026-07-07 18:42:49 -0500

    Update __init__.py

--- src/preprocessing/__init__.py
+++ src/preprocessing/__init__.py
@@ -1,1 +1,35 @@
+"""
+Preprocessing module for tracer transport memory framework.
 
+This module contains functions for:
+
+- BTC normalization
+- concentration processing
+- spatial concentration transformation
+
+"""
+
+from .normalization import (
+    normalize_btc,
+    normalize_spatial_distribution
+)
+
+from .btc_processing import (
+    load_btc,
+    process_btc
+)
+
+from .spatial_processing import (
+    load_spatial_data,
+    process_spatial_concentration
+)
+
+
+__all__ = [
+    "normalize_btc",
+    "normalize_spatial_distribution",
+    "load_btc",
+    "process_btc",
+    "load_spatial_data",
+    "process_spatial_concentration"
+]
