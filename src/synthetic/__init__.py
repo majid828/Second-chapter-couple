06e6480bc197980bf9b63b19788ee80b1a837058
majid828 <mhshah1@crimson.ua.edu>
2026-07-07 18:47:29 -0500

    Update __init__.py

--- src/synthetic/__init__.py
+++ src/synthetic/__init__.py
@@ -1,1 +1,40 @@
+"""
+Synthetic transport experiment generator.
 
+This module provides functions for:
+
+- generating temporal memory functions
+- generating spatial transport kernels
+- creating synthetic concentration fields
+- adding measurement noise
+
+"""
+
+from .generate_memory import (
+    exponential_memory,
+    power_law_memory,
+    hybrid_memory
+)
+
+from .generate_transport import (
+    gaussian_transport_kernel,
+    generate_concentration_field
+)
+
+from .add_noise import (
+    add_gaussian_noise
+)
+
+
+__all__ = [
+
+    "exponential_memory",
+    "power_law_memory",
+    "hybrid_memory",
+
+    "gaussian_transport_kernel",
+    "generate_concentration_field",
+
+    "add_gaussian_noise"
+
+]
