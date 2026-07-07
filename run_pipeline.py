6addb0a8e89a263c7a3837be83b41ea14fdf8f61
majid828 <mhshah1@crimson.ua.edu>
2026-07-07 04:43:50 -0500

    Delete run_pipeline.py

--- run_pipeline.py
+++
@@ -1,9 +0,0 @@
-from src.generator import generate_all
-from src.btc_inverse import recover_kernels
-from src.snapshot_inverse import snapshot_moments
-from src.plotting import make_plots
-
-btc,snap,truth=generate_all()
-rec=recover_kernels(btc)
-print(snapshot_moments(snap))
-make_plots(btc,snap,truth,rec)
