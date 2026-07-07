43f051d42d93c50d774dcb8a0af586651ba48530
majid828 <mhshah1@crimson.ua.edu>
2026-07-07 18:55:02 -0500

    Update __init__.py

--- src/inverse_model/__init__.py
+++ src/inverse_model/__init__.py
@@ -1,1 +1,46 @@
+"""
+Bayesian inverse model module.
 
+Recover temporal memory kernel H(t)
+from observed BTC:
+
+f = K H
+
+using constrained MAP estimation.
+
+"""
+
+from .bayesian_inverse import (
+    recover_memory_kernel
+)
+
+from .map_solver import (
+    solve_map_problem
+)
+
+from .constraints import (
+    positivity_constraint,
+    normalization_constraint
+)
+
+from .regularization import (
+    first_order_regularization,
+    second_order_regularization
+)
+
+
+__all__ = [
+
+    "recover_memory_kernel",
+
+    "solve_map_problem",
+
+    "positivity_constraint",
+
+    "normalization_constraint",
+
+    "first_order_regularization",
+
+    "second_order_regularization"
+
+]
