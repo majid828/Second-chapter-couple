0ef6b02160c0cbd5aef9d7b4377415e0f80ea609
majid828 <mhshah1@crimson.ua.edu>
2026-07-07 18:58:49 -0500

    Update __init__.py

--- src/uncertainty/__init__.py
+++ src/uncertainty/__init__.py
@@ -1,1 +1,39 @@
+"""
+Uncertainty quantification module.
 
+Provides:
+
+- Bayesian posterior covariance
+- Bootstrap confidence intervals
+- Noise sensitivity analysis
+
+"""
+
+from .posterior_covariance import (
+    compute_posterior_covariance,
+    memory_confidence_interval
+)
+
+from .bootstrap import (
+    bootstrap_memory_recovery
+)
+
+from .noise_analysis import (
+    add_relative_noise,
+    noise_sensitivity_test
+)
+
+
+__all__ = [
+
+    "compute_posterior_covariance",
+
+    "memory_confidence_interval",
+
+    "bootstrap_memory_recovery",
+
+    "add_relative_noise",
+
+    "noise_sensitivity_test"
+
+]
