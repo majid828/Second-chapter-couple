0201802d2d960de5929bf0d1b20a1d72b9e7d336
majid828 <mhshah1@crimson.ua.edu>
2026-07-07 19:14:05 -0500

    Update __init__.py

--- src/model_selection/__init__.py
+++ src/model_selection/__init__.py
@@ -1,1 +1,38 @@
+"""
+Model selection module.
 
+Provides:
+
+- Akaike Information Criterion
+- Bayesian Information Criterion
+- Cross-validation comparison
+
+
+Used for selecting the most appropriate
+transport memory mechanism.
+
+"""
+
+
+from .aic import (
+    calculate_aic
+)
+
+from .bic import (
+    calculate_bic
+)
+
+from .cross_validation import (
+    cross_validate_memory_model
+)
+
+
+__all__=[
+
+    "calculate_aic",
+
+    "calculate_bic",
+
+    "cross_validate_memory_model"
+
+]
