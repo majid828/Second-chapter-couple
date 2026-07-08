ffe376a01abfe08d0e7cc36c7a9575d7dde1c8e6
majid828 <mhshah1@crimson.ua.edu>
2026-07-07 20:29:22 -0500

    Update __init__.py

--- experiments/field_sites/__init__.py
+++ experiments/field_sites/__init__.py
@@ -1,1 +1,14 @@
 
+"""
+Field-site experiment workflows.
+
+Each script processes one tracer
+transport experiment site.
+
+Available workflows:
+
+run_site01.py
+run_site02.py
+run_site03.py
+
+"""
