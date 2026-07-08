09d1c7261a2affcce7a0716871b97c46850d1bb9
majid828 <mhshah1@crimson.ua.edu>
2026-07-07 19:28:04 -0500

    Update __init__.py

--- src/validation/__init__.py
+++ src/validation/__init__.py
@@ -1,1 +1,26 @@
+"""
+Validation module.
 
+Contains:
+
+1. Synthetic validation
+2. Field-data validation
+
+"""
+
+from .synthetic_validation import (
+    validate_memory_recovery
+)
+
+from .field_validation import (
+    validate_field_prediction
+)
+
+
+__all__ = [
+
+    "validate_memory_recovery",
+
+    "validate_field_prediction"
+
+]
