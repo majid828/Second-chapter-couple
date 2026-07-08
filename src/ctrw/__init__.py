3b35f8ba8765a8195e25f3dbb0897340cc6a0079
majid828 <mhshah1@crimson.ua.edu>
2026-07-07 19:18:19 -0500

    Update __init__.py

--- src/ctrw/__init__.py
+++ src/ctrw/__init__.py
@@ -1,1 +1,40 @@
+"""
+Continuous-Time Random Walk (CTRW) analysis module.
 
+Provides:
+
+- power-law tail estimation
+- waiting-time distribution analysis
+- CTRW-memory comparison
+
+
+Used for interpreting recovered memory functions
+within anomalous transport theory.
+
+"""
+
+
+from .ctrw_analysis import (
+
+    estimate_power_law_exponent,
+
+    generate_waiting_time_distribution,
+
+    ctrw_memory_comparison,
+
+    tail_statistics
+
+)
+
+
+__all__=[
+
+    "estimate_power_law_exponent",
+
+    "generate_waiting_time_distribution",
+
+    "ctrw_memory_comparison",
+
+    "tail_statistics"
+
+]
