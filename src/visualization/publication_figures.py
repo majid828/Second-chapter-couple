7e69a8ad943bbb785678cad8f494ec074be25c76
majid828 <mhshah1@crimson.ua.edu>
2026-07-07 19:31:16 -0500

    Update publication_figures.py

--- src/visualization/publication_figures.py
+++ src/visualization/publication_figures.py
@@ -1,1 +1,35 @@
+"""
+Publication figure helpers.
 
+Creates combined manuscript figures.
+
+"""
+
+import matplotlib.pyplot as plt
+
+
+
+
+
+def save_publication_figure(
+        filename,
+        dpi=600
+):
+    """
+    Save current matplotlib figure.
+
+    Recommended:
+
+    600 dpi for journals.
+
+    """
+
+    plt.savefig(
+
+        filename,
+
+        dpi=dpi,
+
+        bbox_inches="tight"
+
+    )
