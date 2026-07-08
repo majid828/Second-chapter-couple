6ca33e3756b1ea569f7ecf745cdef23f86b9c4a0
majid828 <mhshah1@crimson.ua.edu>
2026-07-07 21:37:38 -0500

    Update test_memory_models.py

--- tests/test_memory_models.py
+++ tests/test_memory_models.py
@@ -7,103 +7,87 @@ import numpy as np
 
 
 from src.memory_models.exponential import (
-    exponential_memory
+    exponential_model
 )
-
 
 from src.memory_models.power_law import (
-    power_law_memory
+    power_law_model
 )
-
 
 from src.memory_models.hybrid_memory import (
-    hybrid_memory
+    hybrid_model
 )
 
 
 
-def test_exponential_memory():
+def test_exponential_model():
 
-    t=np.linspace(
+    time = np.linspace(
         0,
         10,
         100
     )
 
 
-    H=exponential_memory(
-
-        t,
-
-        amplitude=1,
-
-        decay=0.2
-
+    H = exponential_model(
+        time,
+        A=1.0,
+        beta=0.2
     )
 
 
-    assert len(H)==len(t)
+    assert len(H)==len(time)
 
     assert np.all(
-        H>=0
+        H >= 0
     )
 
 
 
-def test_power_law_memory():
+def test_power_law_model():
 
-    t=np.linspace(
+    time=np.linspace(
         1,
         10,
         100
     )
 
 
-    H=power_law_memory(
-
-        t,
-
-        amplitude=1,
-
+    H = power_law_model(
+        time,
+        A=1.0,
         alpha=0.5
-
     )
 
 
     assert len(H)==100
 
     assert np.all(
-        H>0
+        H > 0
     )
 
 
 
-def test_hybrid_memory():
+def test_hybrid_model():
 
-    t=np.linspace(
+    time=np.linspace(
         0,
         10,
         100
     )
 
 
-    H=hybrid_memory(
-
-        t,
-
+    H = hybrid_model(
+        time,
         A1=0.5,
-
         beta=0.1,
-
         A2=0.5,
-
         alpha=0.5
-
     )
 
 
     assert len(H)==100
 
     assert np.all(
-        H>=0
+        H >= 0
     )
