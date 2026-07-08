6d9df24809848ab8a572bc0e9bbd4ed9cda65943
majid828 <mhshah1@crimson.ua.edu>
2026-07-07 19:09:26 -0500

    Update __init__.py

--- src/memory_models/__init__.py
+++ src/memory_models/__init__.py
@@ -1,1 +1,46 @@
+"""
+Physical transport memory models.
 
+Models:
+
+1. Exponential memory
+
+2. Power-law memory
+
+3. Hybrid memory
+
+
+Used for identifying transport mechanisms
+from recovered H(t).
+
+"""
+
+
+from .exponential import (
+    exponential_model
+)
+
+from .power_law import (
+    power_law_model
+)
+
+from .hybrid_memory import (
+    hybrid_model
+)
+
+from .fit_memory_models import (
+    fit_memory_model
+)
+
+
+__all__=[
+
+    "exponential_model",
+
+    "power_law_model",
+
+    "hybrid_model",
+
+    "fit_memory_model"
+
+]
