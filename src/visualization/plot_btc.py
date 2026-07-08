83dc61e5f9ff8760e294fb76dfc4f5c0ef2bd58d
majid828 <mhshah1@crimson.ua.edu>
2026-07-07 19:29:44 -0500

    Update plot_btc.py

--- src/visualization/plot_btc.py
+++ src/visualization/plot_btc.py
@@ -1,1 +1,57 @@
+"""
+BTC visualization.
 
+"""
+
+import matplotlib.pyplot as plt
+
+
+
+
+
+def plot_btc(
+        time,
+        concentration,
+        normalized=None
+):
+    """
+    Plot breakthrough curve.
+
+    """
+
+    plt.figure(
+        figsize=(7,4)
+    )
+
+
+    plt.plot(
+        time,
+        concentration,
+        label="BTC"
+    )
+
+
+    if normalized is not None:
+
+        plt.plot(
+            time,
+            normalized,
+            label="Normalized BTC"
+        )
+
+
+    plt.xlabel(
+        "Time"
+    )
+
+    plt.ylabel(
+        "Concentration"
+    )
+
+
+    plt.legend()
+
+    plt.tight_layout()
+
+
+    plt.show()
