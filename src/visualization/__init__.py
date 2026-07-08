63964876da06d1dbe948a3e503959904051f2f2a
majid828 <mhshah1@crimson.ua.edu>
2026-07-07 19:29:18 -0500

    Update __init__.py

--- src/visualization/__init__.py
+++ src/visualization/__init__.py
@@ -1,1 +1,40 @@
+"""
+Publication visualization module.
 
+Creates:
+
+- BTC plots
+- Memory plots
+- Kernel plots
+- Cluster plots
+
+"""
+
+from .plot_btc import (
+    plot_btc
+)
+
+from .plot_memory import (
+    plot_memory
+)
+
+from .plot_kernels import (
+    plot_kernel
+)
+
+from .plot_clusters import (
+    plot_clusters
+)
+
+
+__all__=[
+
+    "plot_btc",
+
+    "plot_memory",
+
+    "plot_kernel",
+
+    "plot_clusters"
+
+]
