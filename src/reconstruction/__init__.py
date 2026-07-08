b8b4ab4f2a12d62a26aff4791e7d65116467e785
majid828 <mhshah1@crimson.ua.edu>
2026-07-07 19:06:06 -0500

    Update __init__.py

--- src/reconstruction/__init__.py
+++ src/reconstruction/__init__.py
@@ -1,1 +1,53 @@
+"""
+Plume reconstruction module.
 
+Combines:
+
+Spatial transport kernel:
+    G(x,t)
+
+Temporal memory:
+    H(t)
+
+to reconstruct:
+
+C(x,t)
+
+using:
+
+C(x,t)=
+integral G(x,t-tau)H(tau)d tau
+
+"""
+
+from .plume_reconstruction import (
+    reconstruct_plume,
+    reconstruct_single_time
+)
+
+from .error_metrics import (
+    rmse,
+    nrmse,
+    mae,
+    r2_score,
+    reconstruction_metrics
+)
+
+
+__all__ = [
+
+    "reconstruct_plume",
+
+    "reconstruct_single_time",
+
+    "rmse",
+
+    "nrmse",
+
+    "mae",
+
+    "r2_score",
+
+    "reconstruction_metrics"
+
+]
