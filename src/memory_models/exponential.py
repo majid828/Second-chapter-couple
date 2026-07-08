10deaee9eac7611153bb1edc974c52d3a4995b32
majid828 <mhshah1@crimson.ua.edu>
2026-07-07 19:09:49 -0500

    Update exponential.py

--- src/memory_models/exponential.py
+++ src/memory_models/exponential.py
@@ -1,1 +1,73 @@
+"""
+Exponential memory model.
 
+Equation:
+
+H(t)=A exp(-beta t)
+
+Represents:
+
+- mobile-immobile exchange
+- first order mass transfer
+"""
+
+import numpy as np
+
+
+
+def exponential_model(
+        time,
+        A,
+        beta
+):
+    """
+    Calculate exponential memory.
+
+
+    Parameters
+    ----------
+    time :
+        time array
+
+
+    A :
+        amplitude
+
+
+    beta :
+        decay coefficient
+
+
+
+    Returns
+    -------
+
+    H(t)
+
+    """
+
+    time=np.asarray(
+        time,
+        dtype=float
+    )
+
+
+    if beta <=0:
+
+        raise ValueError(
+            "beta must be positive."
+        )
+
+
+    H = (
+
+        A *
+
+        np.exp(
+            -beta*time
+        )
+
+    )
+
+
+    return H
