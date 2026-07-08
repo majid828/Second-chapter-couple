35a5f6428cc06858341415495e14c279fbff9e02
majid828 <mhshah1@crimson.ua.edu>
2026-07-07 19:30:31 -0500

    Update plot_kernels.py

--- src/visualization/plot_kernels.py
+++ src/visualization/plot_kernels.py
@@ -1,1 +1,50 @@
+"""
+Kernel visualization.
 
+"""
+
+import matplotlib.pyplot as plt
+
+
+
+
+
+def plot_kernel(
+        x,
+        kernel,
+        label="Kernel"
+):
+    """
+    Plot transport kernel.
+
+    """
+
+    plt.figure(
+        figsize=(7,4)
+    )
+
+
+    plt.plot(
+        x,
+        kernel,
+        label=label
+    )
+
+
+    plt.xlabel(
+        "Position"
+    )
+
+
+    plt.ylabel(
+        "Probability density"
+    )
+
+
+    plt.legend()
+
+
+    plt.tight_layout()
+
+
+    plt.show()
