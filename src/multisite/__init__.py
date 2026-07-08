85ccdbb09fb19c805385286ed6c88e783b2a108a
majid828 <mhshah1@crimson.ua.edu>
2026-07-07 19:21:48 -0500

    Update __init__.py

--- src/multisite/__init__.py
+++ src/multisite/__init__.py
@@ -1,1 +1,48 @@
+"""
+Multi-site transport memory classification module.
 
+Functions:
+
+- Wasserstein similarity
+- FPCA dimensional reduction
+- Dynamic time warping
+- Memory clustering
+
+
+Used for discovering transferable
+transport memory classes.
+
+"""
+
+
+from .wasserstein import (
+    wasserstein_distance_matrix
+)
+
+from .fpca import (
+    perform_fpca
+)
+
+from .dtw import (
+    dtw_distance,
+    dtw_distance_matrix
+)
+
+from .clustering import (
+    cluster_memory_functions
+)
+
+
+__all__=[
+
+    "wasserstein_distance_matrix",
+
+    "perform_fpca",
+
+    "dtw_distance",
+
+    "dtw_distance_matrix",
+
+    "cluster_memory_functions"
+
+]
